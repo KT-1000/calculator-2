8163b0123e6cdef46d7ef98bde86adfadb32d0c1
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:21:36 -0700

    Completed arithmetic functions for prefix calculator.

---
+++ arithmetic.py
@@ -0,0 +1,31 @@
+def add(num1, num2):
+    """Add two numbers."""
+    return num1 + num2
+
+def subtract(num1, num2):
+    """Subtract num2 from num1."""
+    return num1 - num2
+
+def multiply(num1, num2):
+    """Multiply num1 and num2."""
+    return num1 * num2
+
+def divide(num1, num2):
+    """Divide num1 by num2."""
+    return num1 / num2
+
+def square(num1): 
+    """Return square of num1."""
+    return num1**2
+
+def cube(num1):
+    """Return cube of num1."""
+    return num1**3
+
+def power(num1, num2):
+    """Raise num1 to the power of num2."""
+    return num1**num2
+
+def mod(num1, num2):
+    """Return the remainder of num1 divided by num2."""
+    return num1 % num2
